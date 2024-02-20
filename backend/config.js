66217730f22b3eb87b9350893a0ca6e965cbfe96
aryanarora07 <aroraaryan824@gmail.com>
2024-02-20 00:52:34 -0800

    coded the connection to the DB

---
+++ backend/config.js
@@ -0,0 +1,1 @@
+export const mongoDBurl = "mongodb+srv://aryan:books123@cluster0.t0dcgix.mongodb.net/books_collection?retryWrites=true&w=majority"
---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import experss from "express";
+import {mongoDBurl} from "./config.js";
+import mongoose from "mongoose";
+import { error } from "console";
+
+const app = experss();
+
+app.get('/', (request, response)=>{
+
+    return response.status(234).send("kiddan jatto");
+})
+
+mongoose.connect(mongoDBurl)
+.then(()=>{
+    console.log("App connected to the database");
+    app.listen(3000);
+})
+.catch((error)=>{
+    console.log(error);
+})
+
