35b61f592a4db1e63fddb3587590d0f6df7752d9
aryanarora07 <aroraaryan824@gmail.com>
2024-02-20 04:56:24 -0800

    starting the frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import Spinner from '../components/Spinner'
+import { Link } from 'react-router-dom'
+import {AiOutlineEdit} from 'react-icons/ai'
+import {BsInfoCircle} from 'react-icons/bs'
+import {MdOutlineAddBox, MdOutlineDelete} from 'react-icons/md'
+
+
+const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(()=>{
+    setLoading(true);
+    axios.get('http://localhost:3000/books')
+  }, [])
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
